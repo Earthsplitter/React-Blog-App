77c634973141a77315ed3090780f3c5c5b1e7359
Ming Wen <wmcs@mail.ustc.edu.cn>
2017-01-29 20:16:52 +0800

    refactor: use ReactCSSTransition to implement animation

--- components/SideBar/SideBar.js
+++ components/SideBar/SideBar.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router'
 
 import SideNav from './SideNav'
 import Information from './Information'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 /**
  * A side bar shown on left, including introduction and navigation
@@ -14,16 +15,10 @@ class SideBar extends React.Component {
 
     constructor(props) {
         super(props);
-        /**
-         * Used to implement fade in transition
-         * @type {{opacity: number, canBeClick: boolean}}
-         */
         this.state = {
-            opacity: 1,                     //current opacity, change from 0 to 1 to fulfill fade in
             canBeClick: true,               //Can not be clicked when sideBar in transition
         };
 
-        this.fadeIn = this.fadeIn.bind(this);
     }
 
     /**
@@ -31,43 +26,27 @@ class SideBar extends React.Component {
      * @param nextProps The next gradient style
      */
     componentWillReceiveProps(nextProps) {
-        if (this.props.gradient.sideBar !== nextProps.gradient.sideBar) {
             this.setState({
-                opacity: 0.1,
                 canBeClick: false,
             });
-            this.timeInterval = setInterval(this.fadeIn,16);
-        }
-    }
-
-    /**
-     *
-     */
-    fadeIn() {
-        this.setState((prevState) => {
-            if (prevState.opacity >= 1) {
-                clearInterval(this.timeInterval);
-                return {
-                    opacity: 1,
-                    canBeClick: true
-                }
-            } else {
-                return {
-                    opacity: prevState.opacity + 0.016,
-                }
-            }
-        });
+            setTimeout(() => (
+                this.setState({
+                    canBeClick:true
+                })
+            ),1000)
     }
 
     render() {
         return (
-            <aside onClick={this.state.canBeClick?this.props.handleClick:''} className="sideBar" style={{
-                opacity:this.state.opacity, height: innerHeight, width: this.props.width, top: '0',
-                background: "linear-gradient(to left," + this.props.gradient.sideBar[0] + "," + this.props.gradient.sideBar[1]}}>
-                <Information/>
-                <SideNav buttonGradient={this.props.gradient.navButton}/>
-                <About/>
-            </aside>
+            <ReactCSSTransitionGroup transitionName="fade" transitionEnterTimeout={1000} transitionLeave={false}>
+                <aside key={this.props.gradient.sideBar[0]} onClick={this.state.canBeClick?this.props.handleClick:''} className="sideBar" style={{
+                    opacity:this.state.opacity, height: innerHeight, width: this.props.width, top: '0',
+                    background: "linear-gradient(to left," + this.props.gradient.sideBar[0] + "," + this.props.gradient.sideBar[1]}}>
+                    <Information/>
+                    <SideNav buttonGradient={this.props.gradient.navButton}/>
+                    <About/>
+                </aside>
+            </ReactCSSTransitionGroup>
         )
     }
 }
